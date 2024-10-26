0756eb294b29a4b644ab920bad631077f925ea27
CʏʙᴇʀKɪʟʟᴇʀꜱ Tᴇᴀᴍ 👽 <161101124+CyberKillersTEAM@users.noreply.github.com>
2024-10-26 15:32:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=yqIGCaaI#3rnxHBIF81twaONiFRFvCDv70xunQpz_582Rnr8CKLM",
 MONGODB: process.env.MONGODB || "mongodb+srv://athulakumara604:qBwvqo6IM64eT1SL@cluster0.wr7rx.mongodb.net/",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
